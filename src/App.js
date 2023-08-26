29e97e1292d0e5a8d6761a8e184aa723a0ad9384
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-26 18:47:29 +0200

    Roma Empire h1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { useState,useEffect } from 'react';
-import './ScrollIndicator.css'
-import './App.css';
-
-
+import { useState, useEffect } from "react";
+import "./ScrollIndicator.css";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-
-
+      <div className="progressMainWrapper">
+        <div className="progressMainStyle"></div>
+      </div>
+      <h1>Roma İmparatorluğu</h1>
     </div>
   );
 }
