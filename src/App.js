4f5f27ef5a03896f55ec2152d156dedacd0881b2
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-26 19:06:03 +0200

    addEventListener

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ const [scrollTop,setScrollTop] = useState(0);
 const onScroll = ()=>{
   const winScroll = document.documentElement.scrollTop;
   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+const scrolled = (winScroll / height) *100;
+
+
+setScrollTop(scrolled);
+
 }
+
+useEffect(()=>{
+  window.addEventListener("scroll",onscroll);
+})
+
 
   return (
     <div className="App">
